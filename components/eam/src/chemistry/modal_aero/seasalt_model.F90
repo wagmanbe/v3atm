3db1424a2b212c074359f88d9c34ddb2356c95d8
Fnu Quazi Ziaur Rasool <quaz115@compy01.pnl.gov>
2022-09-29 18:37:21 -0700

    Added MODAL_AERO_4MODE_SOA_MOM to components/eam/src/chemistry/modal_aero/seasalt_model.F90

--- components/eam/src/chemistry/modal_aero/seasalt_model.F90
+++ components/eam/src/chemistry/modal_aero/seasalt_model.F90
@@ -32,14 +32,14 @@ module seasalt_model
 
 #if  ( defined MODAL_AERO_9MODE )
   integer, parameter :: nslt = 4
-#elif ( ( defined MODAL_AERO_4MODE_MOM ) && ( defined MOSAIC_SPECIES ) )
+#elif ( ( defined MODAL_AERO_4MODE_MOM || defined MODAL_AERO_4MODE_SOA_MOM ) && ( defined MOSAIC_SPECIES ) )
   integer, parameter :: nslt = 9
 #elif ( defined MODAL_AERO_5MODE)
   integer, parameter :: nslt = 3  !kzm
 #else
   integer, parameter :: nslt = max(3,ntot_amode-3)
 #endif
-#if ( ( defined MODAL_AERO_4MODE_MOM ) && ( defined MOSAIC_SPECIES ) )
+#if ( ( defined MODAL_AERO_4MODE_MOM || defined MODAL_AERO_4MODE_SOA_MOM ) && ( defined MOSAIC_SPECIES ) )
   integer, parameter :: nnum = 3
 #else
   integer, parameter :: nnum = nslt
@@ -60,7 +60,7 @@ module seasalt_model
        (/ 'ncl_a1', 'ncl_a2', 'ncl_a3', &
           'num_a1', 'num_a2', 'num_a3'/)
   integer, parameter :: om_num_ind = 0
-#elif ( ( defined MODAL_AERO_4MODE_MOM ) && ( defined MOSAIC_SPECIES ) )
+#elif ( ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM) && ( defined MOSAIC_SPECIES ) )
   integer, parameter :: nslt_om = 3
   integer, parameter :: nnum_om = 1
   integer, parameter :: om_num_modes = 3
@@ -71,7 +71,7 @@ module seasalt_model
           'mom_a1', 'mom_a2', 'mom_a4', &
           'num_a1', 'num_a2', 'num_a3', 'num_a4'/)
   integer, dimension(om_num_modes), parameter :: om_num_ind =  (/ 1, 2, 4 /)
-#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE)
+#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE)
   integer, parameter :: nslt_om = 3
   integer, parameter :: nnum_om = 1
   integer, parameter :: om_num_modes = 3
@@ -159,7 +159,8 @@ module seasalt_model
    integer             :: fmoa = 1
 
 ! TODO SMB: Implement better mechanism for setting this switch.
-#if (defined MODAL_AERO_9MODE || defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE)
+! QZR added MODAL_AERO_4MODE_SOA_MOM
+#if (defined MODAL_AERO_9MODE || defined MODAL_AERO_4MODE_MOM || defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE)
    logical :: has_mam_mom = .true.
 #else
    logical :: has_mam_mom = .false.
@@ -195,7 +196,7 @@ module seasalt_model
          (/ 0.08e-6_r8,  0.02e-6_r8,  1.0e-6_r8 /)  ! accu, aitken, coarse
     real(r8), parameter :: sst_sz_range_hi (nslt+nslt_om) = &
          (/ 1.0e-6_r8,   0.08e-6_r8, 10.0e-6_r8 /)  ! accu, aitken, coarse
-#elif ( ( defined MODAL_AERO_4MODE_MOM ) && ( defined MOSAIC_SPECIES ) )
+#elif ( ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ) && ( defined MOSAIC_SPECIES ) )
     real(r8), parameter :: sst_sz_range_lo (nslt+nslt_om) = &
          (/ 0.08e-6_r8,  0.02e-6_r8,  1.0e-6_r8, &  ! accu, aitken, coarse
             0.08e-6_r8,  0.02e-6_r8,  1.0e-6_r8, &
@@ -206,7 +207,7 @@ module seasalt_model
             1.0e-6_r8,   0.08e-6_r8, 10.0e-6_r8, &
             1.0e-6_r8,   0.08e-6_r8, 10.0e-6_r8, &
             1.0e-6_r8,   0.08e-6_r8,  1.0e-6_r8 /)  ! accu, aitken, POM accu
-#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE )
+#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE )
     real(r8), parameter :: sst_sz_range_lo (nslt+nslt_om) = &
          (/ 0.08e-6_r8,  0.02e-6_r8,  1.0e-6_r8, &  ! accu, aitken, coarse
             0.08e-6_r8,  0.02e-6_r8,  0.08e-6_r8 /) ! accu, aitken, POM accu
@@ -653,7 +654,7 @@ end subroutine ocean_data_readnl
 
 enddo tracer_loop
 
-#if ( defined MODAL_AERO_9MODE || defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE)
+#if ( defined MODAL_AERO_9MODE || defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE)
 
 add_om_species: if ( has_mam_mom ) then
 ! Calculate emission of MOM mass.
@@ -685,7 +686,7 @@ add_om_species: if ( has_mam_mom ) then
     else
        call endrun("Error: Unknown mixing_state value in seasalt_model.F90")
     end if
-#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE )
+#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE )
     if ((mixing_state == 1) .or. (mixing_state == 0)) then
        emit_this_mode = (/ .false., .true., .true. /)
     else if ((mixing_state == 2) .or. (mixing_state == 3)) then
@@ -738,7 +739,7 @@ add_om_species: if ( has_mam_mom ) then
     om_mode_loop: do m_om=1,nslt_om
 #if ( defined MODAL_AERO_9MODE )
        mm = seasalt_indices(nslt+(n-1)*om_num_modes+m_om)
-#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE)
+#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE)
        mm = seasalt_indices(nslt+m_om)
 #endif
 
@@ -1272,7 +1273,7 @@ subroutine init_ocean_data()
        om_mode_loop: do m_om=1,nslt_om
 #if ( defined MODAL_AERO_9MODE )
           m = nslt+(n-1)*om_num_modes+m_om
-#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_5MODE)
+#elif ( defined MODAL_AERO_4MODE_MOM ||  defined MODAL_AERO_4MODE_SOA_MOM ||  defined MODAL_AERO_5MODE)
           m = nslt+m_om
 #endif
           call addfld('cflx_'//trim(seasalt_names(m))//'_debug', horiz_only, 'A', ' ', 'accumulation organic mass emissions' ) 
