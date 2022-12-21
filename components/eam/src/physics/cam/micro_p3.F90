9ec3c4c53dec6f0737b12b096936a5d777440736
Yunpeng Shan <ac.yshan@chrlogin1.lcrc.anl.gov>
2022-12-21 12:31:50 -0600

    Tunable low nc change highlighted. Add mpibcast

--- components/eam/src/physics/cam/micro_p3.F90
+++ components/eam/src/physics/cam/micro_p3.F90
@@ -1082,7 +1082,7 @@ end function bfb_expm1
       rho, inv_rho, rhofaci, qv, th_atm, qc, nc, qr, nr, qi, ni, qm, bm, latent_heat_vapor, latent_heat_sublim,            &
       mu_c, nu, lamc, mu_r, lamr, vap_liq_exchange,                                                                        &
       ze_rain, ze_ice, diag_vm_qi, diag_eff_radius_qi, diag_diam_qi, rho_qi, diag_equiv_reflectivity, diag_eff_radius_qc,  & 
-!<shanyp 11212022
+!<shanyp-LowNc 11212022
       diag_ze_rain,diag_ze_ice,mincdnc)
 !shanyp 11212022>
 
@@ -1091,7 +1091,7 @@ end function bfb_expm1
    ! args
 
    integer, intent(in) :: kts, kte, kbot, ktop, kdir
-!<shanyp 11212022
+!<shanyp-LowNc 11212022
    real(rtype), intent(in) :: p3_max_mean_rain_size,mincdnc
 !shanyp 11212022>
 
@@ -1132,7 +1132,7 @@ end function bfb_expm1
          nc_incld = nc(k)/cld_frac_l(k)
          call get_cloud_dsd2(qc_incld,nc_incld,mu_c(k),rho(k),nu(k),dnu,lamc(k),  &
               tmp1,tmp2)
-!<shanyp 07112022
+!<shanyp-LowNc 07112022
        if (mincdnc.gt.0._rtype) nc_incld = max(nc_incld,mincdnc/rho(k))
 !       write(iulog, *) "LLILY", mincdnc
 !<shanyp 11212022
@@ -1260,7 +1260,7 @@ end function bfb_expm1
   SUBROUTINE p3_main(qc,nc,qr,nr,th_atm,qv,dt,qi,qm,ni,bm,                                                                                                               &
        pres,dz,nc_nuceat_tend,nccn_prescribed,ni_activated,frzimm,frzcnt,frzdep,inv_qc_relvar,it,precip_liq_surf,precip_ice_surf,its,ite,kts,kte,diag_eff_radius_qc,     &
        diag_eff_radius_qi,rho_qi,do_predict_nc, do_prescribed_CCN,p3_autocon_coeff,p3_accret_coeff,p3_qc_autocon_expon,p3_nc_autocon_expon,p3_qc_accret_expon,           &
-!<shanyp 11052022
+!<shanyp-LowNc 11052022
        p3_wbf_coeff,p3_mincdnc,p3_max_mean_rain_size,p3_embryonic_rain_size,                                                                                             &
 !shanyp 11052022>
        dpres,exner,qv2qi_depos_tend,precip_total_tend,nevapr,qr_evap_tend,precip_liq_flux,precip_ice_flux,rflx,sflx,cflx,cld_frac_r,cld_frac_l,cld_frac_i,               &
@@ -1350,7 +1350,7 @@ end function bfb_expm1
     real(rtype), intent(in)                                     :: p3_nc_autocon_expon      ! autconversion nc exponent
     real(rtype), intent(in)                                     :: p3_qc_accret_expon       ! accretion qc and qr exponent
     real(rtype), intent(in)                                     :: p3_wbf_coeff             ! WBF coefficient
-!<shanyp 11052022
+!<shanyp-LowNc 11052022
     real(rtype), intent(in)                                     :: p3_mincdnc               ! impose minimal Nc
 !shanyp 11052022>
     real(rtype), intent(in)                                     :: p3_max_mean_rain_size    ! max mean rain size allowed
@@ -1417,7 +1417,7 @@ end function bfb_expm1
     logical(btype), parameter :: debug_ABORT  = .false.  !.true. will result in forced abort in s/r 'check_values'
 
     real(rtype),dimension(its:ite,kts:kte) :: qc_old, nc_old, qr_old, nr_old, qi_old, ni_old, qv_old, th_atm_old
-!<shanyp 07102022
+!<shanyp-LowNc 07102022
     integer :: knc
     real(rtype) :: mincdnc
 !shanyp 07102022>
@@ -1501,7 +1501,7 @@ end function bfb_expm1
     ni_old = ni   ! Ice  # microphysics tendency, initialize
     qv_old = qv         ! Vapor  microphysics tendency, initialize
     th_atm_old = th_atm         ! Pot. Temp. microphysics tendency, initialize
-!<shanyp 11212022
+!<shanyp-LowNc 11212022
     mincdnc=p3_mincdnc
 !shanyp 11212022>
 
@@ -1638,7 +1638,7 @@ end function bfb_expm1
        ! Instantenous melting of ice/snow at T = t_snow_melt = 2c    
        call ice_complete_melting(kts,kte,ktop,kbot,kdir,qi(i,:),ni(i,:),qm(i,:),latent_heat_fusion(i,:),exner(i,:),th_atm(i,:), & 
             qr(i,:),nr(i,:),qc(i,:),nc(i,:))
-!<shanyp 07112022
+!<shanyp-LowNc 07112022
          do knc=kbot,ktop,kdir
           if ((mincdnc.gt.0._rtype).and.(qc(i,knc).ge.qsmall)) then
            nc(i,knc) = max(nc(i,knc),mincdnc*cld_frac_l(i,knc)/rho(i,knc))
@@ -1656,7 +1656,7 @@ end function bfb_expm1
             qm(i,:), bm(i,:), latent_heat_vapor(i,:), latent_heat_sublim(i,:),                                                &
             mu_c(i,:), nu(i,:), lamc(i,:), mu_r(i,:), lamr(i,:), vap_liq_exchange(i,:),                                       &
             ze_rain(i,:), ze_ice(i,:), diag_vm_qi(i,:), diag_eff_radius_qi(i,:), diag_diam_qi(i,:), rho_qi(i,:), diag_equiv_reflectivity(i,:), diag_eff_radius_qc(i,:), &
-!<shanyp 11212022
+!<shanyp-LowNc 11212022
             diag_ze_rain(i,:),diag_ze_ice(i,:),mincdnc)
 !shanyp 11212022>
 
--- components/eam/src/physics/cam/micro_p3_interface.F90
+++ components/eam/src/physics/cam/micro_p3_interface.F90
@@ -129,7 +129,7 @@ module micro_p3_interface
       p3_nc_autocon_expon      = huge(1.0_rtype), &
       p3_qc_accret_expon       = huge(1.0_rtype), &
       p3_wbf_coeff             = huge(1.0_rtype), &
-!<shanyp 11052022
+!<shanyp-LowNc 11052022
       p3_mincdnc               = huge(1.0_rtype), &
 !shanyp 11052022>
       p3_max_mean_rain_size    = huge(1.0_rtype), &
@@ -167,7 +167,7 @@ subroutine micro_p3_readnl(nlfile)
        micro_p3_tableversion, micro_p3_lookup_dir, micro_aerosolactivation, micro_subgrid_cloud, &
        micro_tend_output, p3_autocon_coeff, p3_qc_autocon_expon, p3_nc_autocon_expon, p3_accret_coeff, &
        p3_qc_accret_expon, p3_wbf_coeff, p3_max_mean_rain_size, p3_embryonic_rain_size, &
-!<shanyp 11052022
+!<shanyp-LowNc 11052022
        do_prescribed_CCN, do_Cooper_inP3, p3_mincdnc
 !shanyp 11052022>
   !-----------------------------------------------------------------------------
@@ -197,6 +197,9 @@ subroutine micro_p3_readnl(nlfile)
      write(iulog,'(A30,1x,8e12.4)') 'p3_nc_autocon_expon',     p3_nc_autocon_expon
      write(iulog,'(A30,1x,8e12.4)') 'p3_qc_accret_expon',      p3_qc_accret_expon
      write(iulog,'(A30,1x,8e12.4)') 'p3_wbf_coeff',            p3_wbf_coeff
+!<shanyp-LowNc 11052022
+     write(iulog,'(A30,1x,8e12.4)') 'p3_mincdnc',              p3_mincdnc
+!shanyp 11052022>
      write(iulog,'(A30,1x,8e12.4)') 'p3_max_mean_rain_size',   p3_max_mean_rain_size
      write(iulog,'(A30,1x,8e12.4)') 'p3_embryonic_rain_size',  p3_embryonic_rain_size
      write(iulog,'(A30,1x,L)')    'do_prescribed_CCN: ',       do_prescribed_CCN
@@ -217,6 +220,9 @@ subroutine micro_p3_readnl(nlfile)
   call mpibcast(p3_accret_coeff,         1 ,                         mpir8,   0, mpicom)
   call mpibcast(p3_qc_accret_expon,      1 ,                         mpir8,   0, mpicom)
   call mpibcast(p3_wbf_coeff,            1 ,                         mpir8,   0, mpicom)
+!<shanyp-LowNc 11052022
+  call mpibcast(p3_mincdnc,              1 ,                         mpir8,   0, mpicom)
+!shanyp 11052022>
   call mpibcast(p3_max_mean_rain_size,   1 ,                         mpir8,   0, mpicom)
   call mpibcast(p3_embryonic_rain_size,  1 ,                         mpir8,   0, mpicom)
   call mpibcast(do_prescribed_CCN,       1,                          mpilog,  0, mpicom)
@@ -1328,7 +1334,7 @@ end subroutine micro_p3_readnl
          p3_nc_autocon_expon,         & ! IN  autoconversion nc exponent
          p3_qc_accret_expon,          & ! IN  autoconversion coefficient
          p3_wbf_coeff,                & ! IN  WBF process coefficient
-!<shanyp 11052022
+!<shanyp-LowNc 11052022
          p3_mincdnc,                  & ! IN  imposing minimal Nc
 !shanyp 11052022>
          p3_max_mean_rain_size,       & ! IN  max mean rain size
